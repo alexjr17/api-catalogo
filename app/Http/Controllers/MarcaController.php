56c0d94807bca873cfe03b3c048363014bddbfd8
alexjr17 <alexjose.r.r@gmail.com>
2022-07-17 08:42:22 -0500

    crear, listar, eliminar,editar de marcas - api

---
+++ app/Http/Controllers/MarcaController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Marca;
+use Illuminate\Http\Request;
+
+class MarcaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $marcas = Marca::orderBy('id', 'desc')->paginate(10);
+        return response()->json($marcas);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate(Marca::$rules);
+        // dd($request->all());
+        $marca = Marca::create(
+            [
+                'name' => $request->name
+            ]
+        );
+        return response()->json(['marca' => $marca, 'state' => 200]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Marca $marca)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $marca = Marca::find($id);
+        if($marca){
+            return response()->json($marca);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function update($id, Request $request)
+    {
+        $request->validate(Marca::$rules);
+        $marca = Marca::find($id);
+        if ($marca) {
+            $marca->update($request->all());
+            return response()->json([$marca, 'state' => 200]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $marca = Marca::find($id);
+        if($marca) {
+            $marca->delete();
+            return response()->json([$marca, 'state' => 200]);
+        }
+    }
+}
